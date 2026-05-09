571925eb73a175981a8cd781b77848bb0712d337
thisisyourfahi <fahiyanofficial@gmail.com>
2026-05-09 19:36:17 +0600

    project initialized

---
+++ src/app/news/[id]/page.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const NewsDetailsPage = () => {
+    return (
+        <div>
+            <p>News Details Page</p>
+        </div>
+    );
+};
+
+export default NewsDetailsPage;
---
+++ src/app/register/page.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const RegisterPage = () => {
+    return (
+        <div>
+            <p>Register page</p>
+        </div>
+    );
+};
+
+export default RegisterPage;
